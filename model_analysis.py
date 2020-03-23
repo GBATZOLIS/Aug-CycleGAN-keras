2411fb8aaa1bc353f634e0f2425c14e7e5825d99
gbatzolis1 <gbatzolis1@users.noreply.github.com>
2020-03-23 17:03:18 +0200

    n

---
+++ model_analysis.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 23 16:57:48 2020
+
+@author: Georgios
+"""
+
+#This file is used for custom evaluation of the trained models
+
+#Task1 
+"""Explore the latent space and latent space efficiency"""
+
+"""
+1.) Train a model with a 2D latent space. 
+2.) Evaluate G(A,z(i)) in a 2D grid of z(i) (-3,3)x(-3,3) range
+3.) Evaluate SSIM and LPIPS on the grid points and plot the contour or surface plot
+"""
+
+
